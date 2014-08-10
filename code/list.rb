71faa88cfc0d8582f85e68fe84b1680ceb9b5dce
naiduv <naiduvenkat@gmail.com>
2014-08-10 03:27:44 -0400

    found new ruby files

--- code/list.rb
+++ code/list.rb
@@ -6,7 +6,7 @@
 <meta name="description" content="Matsukei Open Source Center" />
 <meta name="keywords" content="Ruby,帳票,OSS,Rails,マツケイ" />
 <meta content="authenticity_token" name="csrf-param" />
-<meta content="gT9c39JSVi/tXyITz2h9F33rtm0VS/Gp8LxGs0oaiGY=" name="csrf-token" />
+<meta content="fRaHmri6IG31Sea5dJL3xDv67tSPj0Vtjpn6jO+2xD4=" name="csrf-token" />
 <link rel='shortcut icon' href='/favicon.ico?1348113956' />
 <link href="/stylesheets/jquery/jquery-ui-1.9.2.css?1372407541" media="all" rel="stylesheet" type="text/css" />
 <link href="/themes/mosc/stylesheets/application.css?1375373810" media="all" rel="stylesheet" type="text/css" />
