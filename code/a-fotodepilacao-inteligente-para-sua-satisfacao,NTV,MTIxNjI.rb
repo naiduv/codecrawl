ddbf87e5abf4882f2ed47e9636f6e8b51c0e8b9d
naiduv <naiduvenkat@gmail.com>
2014-08-10 04:21:17 -0400

    found new ruby files

--- code/a-fotodepilacao-inteligente-para-sua-satisfacao,NTV,MTIxNjI.rb
+++ code/a-fotodepilacao-inteligente-para-sua-satisfacao,NTV,MTIxNjI.rb
@@ -162,15 +162,11 @@ $(document).ready(function() {
 	<a href="http://www.catalaonoticias.com.br/voce-reporter/enviar.html"><img src="banners/vocereporter.jpg" border="0"></a></div>-->
     
 	
-	<li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/wgo_16-04-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+	<li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/ideal_tintas-10-03-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
 
 </ul>
 
-    <li><div style='border: thin solid #999'><a href='http://www.antaresviagens.com.br/' target='_blank'><img src='/portal/noticias/banners/antares1.jpg' width='311' height='153' /></a></div></li>    <ul id="lateral">
-
-</ul>
-
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/peixaria_29-05-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/dispachante_minas_gerais.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
 
 </ul>
 
@@ -178,7 +174,11 @@ $(document).ready(function() {
 
 </ul>
 
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/cantinho_da_crianca_3.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/wgo_16-04-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+
+</ul>
+
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/peixaria_29-05-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
 
 </ul>
 
@@ -199,15 +199,15 @@ $(document).ready(function() {
 
 </ul>
 
-    <li><div style='border: thin solid #999'><a href='' target='_blank'><img src='/portal/noticias/banners/drdaniela.jpg' width='311' height='153' /></a></div></li>    <ul id="lateral">
+    <li><div style='border: thin solid #999'><a href='http://www.antaresviagens.com.br/' target='_blank'><img src='/portal/noticias/banners/antares1.jpg' width='311' height='153' /></a></div></li>    <ul id="lateral">
 
 </ul>
 
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/microtec.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/emporio_catalao_16-05-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
 
 </ul>
 
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/iluminare-eletrica_21-03-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/odonto_freitas_26-06-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
 
 </ul>
 
@@ -235,23 +235,7 @@ $(document).ready(function() {
     </li>
 </ul>
 
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/inusitta_noticias_20-03-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
-
-</ul>
-
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/emporio_catalao_16-05-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
-
-</ul>
-
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/odonto_freitas_26-06-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
-
-</ul>
-
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/studio-07-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
-
-</ul>
-
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/mastermel.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+    <li><div style='border: thin solid #999'><a href='' target='_blank'><img src='/portal/noticias/banners/drdaniela.jpg' width='311' height='153' /></a></div></li>    <ul id="lateral">
 
 </ul>
 
@@ -259,11 +243,27 @@ $(document).ready(function() {
 
 </ul>
 
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/dispachante_minas_gerais.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/studio-07-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
 
 </ul>
 
-    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/ideal_tintas-10-03-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/iluminare-eletrica_21-03-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+
+</ul>
+
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/cantinho_da_crianca_3.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+
+</ul>
+
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/inusitta_noticias_20-03-2014.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+
+</ul>
+
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/mastermel.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
+
+</ul>
+
+    <li><div style='border: thin solid #999; background:url(/portal/noticias/banners/)';><script>exibeFash('/portal/noticias/banners/microtec.swf', '311', '153', 'opaque')</script></div></li>    <ul id="lateral">
 
 </ul>
 
@@ -271,7 +271,7 @@ $(document).ready(function() {
 
 <!-- FINAL BANNERS -->
 <ul id="banners">
-	<li><div style='border: thin solid #999'><a href='' target='_blank'><img src='/portal/noticias/banners/ferreira.jpg' width='311' height='153' /></a></div></li><li><div style='border: thin solid #999'><a href='http://www.jornalocatalao.com.br' target='_blank'><img src='/portal/noticias/banners/ocatalao.jpg' width='311' height='153' /></a></div></li><li><div style='border: thin solid #999'><a href='http://www.cantinasselfservice.com.br/' target='_blank'><img src='/portal/noticias/banners/cantinas.jpg' width='311' height='153' /></a></div></li></ul>
+	<li><div style='border: thin solid #999'><a href='http://www.cantinasselfservice.com.br/' target='_blank'><img src='/portal/noticias/banners/cantinas.jpg' width='311' height='153' /></a></div></li><li><div style='border: thin solid #999'><a href='' target='_blank'><img src='/portal/noticias/banners/ferreira.jpg' width='311' height='153' /></a></div></li><li><div style='border: thin solid #999'><a href='http://www.jornalocatalao.com.br' target='_blank'><img src='/portal/noticias/banners/ocatalao.jpg' width='311' height='153' /></a></div></li></ul>
 
 <!-- FINAL BANNER -->
 
